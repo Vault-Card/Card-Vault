ad50391b198fb5971564ae574d5a49f524cd019c
Isaac Brickner <91433002+isaacbrickner@users.noreply.github.com>
2025-04-08 15:42:04 -0700

    add scaffolding for cloud services using CDK. not yet deployed - waiting on AWS accounts

--- backend/lib/backend-stack.ts
+++ backend/lib/backend-stack.ts
@@ -1,16 +1,32 @@
 import * as cdk from 'aws-cdk-lib';
+import { RestApi } from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class BackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // API Gateway
+    const restApi = new RestApi(this, 'cards-api');
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // DynamoDB
+    const table = new dynamodb.Table(this, 'Table', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      tableClass: dynamodb.TableClass.STANDARD,
+      tableName: 'cards'
+    });
+ 
+    // Lambda
+    const getCardsFunction = new NodejsFunction(this, 'MyFunction', {
+      entry: 'lambda/getCards.ts',
+      handler: 'handler',
+      environment: {
+        TABLE_NAME: table.tableName
+      }
+    });
+
   }
 }
