6789ba07959cf2845990506ab535f437cd11aa48
Isaac Brickner <91433002+isaacbrickner@users.noreply.github.com>
2025-04-12 09:38:41 -0700

    add dataHandler Lambda function and deploy workflow; update dependencies

--- backend/lib/backend-stack.ts
+++ backend/lib/backend-stack.ts
@@ -3,7 +3,9 @@ import { RestApi } from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import path = require('path');
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
 
 export class BackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -21,12 +23,22 @@ export class BackendStack extends cdk.Stack {
  
     // Lambda
     const getCardsFunction = new NodejsFunction(this, 'MyFunction', {
-      entry: 'lambda/getCards.ts',
+      entry: path.join(__dirname, '/lambdas/dataHandler.ts'),
       handler: 'handler',
+      runtime: Runtime.NODEJS_22_X, // ADD THIS LINE
+
+      bundling: {
+        forceDockerBundling: false, // force using local esbuild
+      },
       environment: {
         TABLE_NAME: table.tableName
       }
     });
 
+
+    const cardsResource = restApi.root.addResource('cards');
+
+    // Add a GET method to that resource and link it to your Lambda
+    cardsResource.addMethod('GET', new cdk.aws_apigateway.LambdaIntegration(getCardsFunction));
   }
 }
