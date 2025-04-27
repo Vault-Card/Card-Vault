903ceb97b79307201757bd9c61b17098c01028ca
Curtis Kauer <kauercurtis@gmail.com>
2025-04-27 13:52:53 -0700

    search bar component created

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,37 +1,41 @@
-import React from 'react'
-import { Tabs } from 'expo-router'
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import "@/global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
 
-import DashboardIcon from '../components/ui/customIcons/DashboardIcon';
-import CollectionIcon from '../components/ui/customIcons/CollectionIcon';
-import MarketplaceIcon from '../components/ui/customIcons/MarketplaceIcon';
+import { useColorScheme } from '@/hooks/useColorScheme';
 
-const _layout = () => {
-    return (
-        <Tabs >
-            <Tabs.Screen
-                name = "index"
-                options = {{
-                    title: "Dashboard",
-                    tabBarIcon: ({ size }) => DashboardIcon()
-                }}
-            />
-            <Tabs.Screen
-                name = "collection"
-                options = {{
-                    title: "Collection",
-                    tabBarIcon: ({}) => CollectionIcon()
-                }}
-            />
-            <Tabs.Screen
-                name = "marketplace"
-                options = {{
-                    title: "Marketplace",
-                    tabBarIcon: ({}) => MarketplaceIcon()
-                }}
-            />
-        </Tabs>
-        
-    );
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <GluestackUIProvider mode="light"><ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+        <StatusBar style="auto" />
+      </ThemeProvider></GluestackUIProvider>
+  );
 }
-
-export default _layout
