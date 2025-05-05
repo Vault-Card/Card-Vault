8811a91aeec1d1c5446603742f481d772aa6e251
Isaac Brickner <91433002+isaacbrickner@users.noreply.github.com>
2025-05-05 13:39:30 -0700

    change port and host

--- model/server/main.py
+++ model/server/main.py
@@ -155,4 +155,5 @@ def upload_image():
 
 if __name__ == '__main__':
     #  Run the Flask development server.  For production, use a proper WSGI server (e.g., gunicorn).
-    app.run(debug=True, host='127.0.0.1', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=5000
+    )
